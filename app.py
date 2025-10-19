25e898204d4eb0140a4371ed4d57cb4bd966df7c
YuvrajDs1 <yuvraj1desai@gmail.com>
2025-10-19 12:29:11 +0530

    tweaks

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from dotenv import load_dotenv
 # LangChain / vectorstore imports
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
